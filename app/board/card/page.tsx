7d0129de87e244f4261a1053a424d7b391f5f2fd
lilyl3 <lily.lin.915@gmail.com>
2025-10-15 12:57:48 -0700

    Add a card for each question

--- app/board/card/page.tsx
+++ app/board/card/page.tsx
@@ -1,5 +1,26 @@
-export default function Card() {
+type CardProps = {
+    src?: string
+    points: string
+  }
+  
+export default function Card({ src, points }: CardProps) {
     return (
-        <></>
-    );
-};
+      <div
+        className="border border-gray-300 rounded-lg shadow-md p-4 m-2 w-[175px] flex flex-col items-center text-center cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105 gap-2"
+      >
+        {src ? (
+          <img
+            className="w-full max-h-[120px] object-cover block"
+            src={src}
+            alt="image"
+          />
+        ) : null}
+  
+        <h2 className="text-xl font-semibold m-0 p-0">
+          <a href="#" className="text-inherit hover:underline">
+            {points}
+          </a>
+        </h2>
+      </div>
+    )
+}  
