9d6304d7a61d31588c24ceef748f3320e50b8ff6
lilyl3 <lily.lin.915@gmail.com>
2025-10-17 16:29:56 -0700

    Add incorrect/correct buttons instead of home & fix toggling of buttons

--- app/board/[category]/[pointValue]/qa.tsx
+++ app/board/[category]/[pointValue]/qa.tsx
@@ -22,16 +22,22 @@ export default function ToggleQA({ params }: QAProps) {
         <p className="text-4xl text-gray-900 text-center font-semibold">
             {showAnswer ? answer : question}
         </p>
+        {/* On question page */}
+        {!showAnswer && 
         <button 
             onClick={() => setShowAnswer(!showAnswer)} 
             className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-8 rounded transition"
         >
-            {showAnswer ? "Show Question" : "Show Answer"}
-        </button>
-        <button className="bg-orange-200 hover:bg-orange-300 font-semibold py-3 px-8 rounded transition"
-        onClick={() => goToBoard()}>
-            Home
-        </button>
+            Show Answer
+        </button>}
+        {showAnswer && <div className="flex flex-row gap-10">
+            <button className="bg-red-200 hover:bg-red-300 font-semibold py-3 px-8 rounded transition" onClick={() => goToBoard()}>
+                Incorrect
+            </button>
+            <button className="bg-green-200 hover:bg-green-300 font-semibold py-3 px-8 rounded transition" onClick={() => goToBoard()}>
+                Correct
+            </button>
+        </div>}
         </div>
     );
 }
